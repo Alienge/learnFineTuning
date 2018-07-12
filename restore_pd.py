507403a58f306d94b28d1c0fd10477578d6b4710
Alienge <1311141809@qq.com>
2018-07-12 10:27:41 +0800

    second commit

---
+++ restore_pd.py
@@ -0,0 +1,82 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tensorflow.examples.tutorials.mnist import input_data
+from datetime import datetime
+import os
+
+mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
+import tensorflow as tf
+def _bias_variable(name,shape,initializer):
+    var = tf.get_variable(name, shape, initializer=initializer, dtype=tf.float32)
+    return var
+def _weight_variable(name,shape,std):
+    return _bias_variable(name, shape, initializer=tf.truncated_normal_initializer(stddev=std,dtype=tf.float32),
+                          )
+def inference(input):
+    with tf.variable_scope('layer3') as scope:
+        weights = _weight_variable('weights',[128,64],std=0.001)
+        bias = _bias_variable('bias',[64],tf.constant_initializer(0.0))
+        layer3 = tf.nn.relu(tf.matmul(input, weights) + bias, name=scope.name)
+    with tf.variable_scope('softmax_linear') as scope:
+        weights = _weight_variable('weights', [64, 10], std=1 / 192.0)
+        bias = _bias_variable('bias', [10], tf.constant_initializer(0.0))
+        softmax_linear = tf.add(tf.matmul(layer3, weights), bias, name=scope.name)
+    return softmax_linear
+
+def loss(logits,labels):
+    labels = tf.cast(labels,tf.int64)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.argmax(labels,1),logits=logits,name = 'cross_entropy')
+    cross_entropy_mean  = tf.reduce_mean(cross_entropy,name = 'cross_entropy')
+    return cross_entropy_mean
+
+
+batch_size = 100
+training_epoch = 20
+with tf.Graph().as_default() as g:
+    x_place = tf.placeholder(tf.float32, shape=[None, 784], name='x')
+    y_place = tf.placeholder(tf.float32, shape=[None, 10], name='y')
+    with open('./graph.pb','rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        tf.import_graph_def(graph_def, name='')
+        graph_op = tf.import_graph_def(graph_def,name='',input_map={'input/x:0':x_place},
+                                       return_elements=['layer2/layer2:0','layer1/weights:0'])
+
+   # x_place = g.get_tensor_by_name('input/x:0')
+    #y_place = g.get_tensor_by_name('input/y:0')
+    #layer2 = g.get_tensor_by_name('layer2/layer2:0')
+    #weight_test = g.get_tensor_by_name('layer1/weights:0')
+    #layer2 = g.get_tensor_by_name('layer2/layer2:0')
+    #layer2 = tf.stop_gradient(layer2,name='stop_gradient')
+    layer2 = graph_op[0]
+    weight_test = graph_op[1]
+    soft_result = inference(layer2)
+    cost = loss(soft_result,y_place)
+    opt = tf.train.AdamOptimizer(0.001).minimize(cost)
+    correct_prediction = tf.equal(tf.argmax(y_place, 1), tf.argmax(soft_result, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+with tf.Session(graph=g) as sess:
+    value=[]
+    #saver.restore(sess, tf.train.latest_checkpoint('./'))
+    sess.run(tf.global_variables_initializer())
+    #weight_test = sess.g.get_tensor_by_name('layer1/weights:0')
+    for epoch in range(training_epoch):
+        avg_cost = 0.0
+        total_batch = int(mnist.train.num_examples / batch_size)
+        for _ in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            sess.run(opt, feed_dict={x_place: batch_xs, y_place: batch_ys})
+            cost_ = sess.run(cost, feed_dict={x_place: batch_xs, y_place: batch_ys})
+            weight_test_value = sess.run(weight_test,feed_dict={x_place: batch_xs, y_place: batch_ys})
+        print(("%s epoch: %d,cost: %.6f") % (datetime.now(), epoch + 1, cost_))
+
+        if (epoch+1) % 5 == 0:
+            value.append(weight_test_value)
+    for i in range(len(value)-1):
+        if value[i].all()==value[i+1].all():
+            print("weight is equal")
+    mean_accuary = sess.run(accuracy, {x_place: mnist.test.images, y_place: mnist.test.labels})
+    print("accuracy %3.f" % mean_accuary)
+
---
+++ save_pd.py
@@ -0,0 +1,83 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from tensorflow.examples.tutorials.mnist import input_data
+from datetime import datetime
+from tensorflow.python.framework.graph_util import convert_variables_to_constants
+import os
+mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
+
+import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_integer('batch_size',100,'''batch_size''')
+tf.app.flags.DEFINE_integer('traing_epoches',15,'''epoch''')
+tf.app.flags.DEFINE_string('check_point_dir','./','check_ponint_dir')
+def _bias_variable(name,shape,initializer):
+    var = tf.get_variable(name, shape, initializer=initializer, dtype=tf.float32)
+    return var
+def _weight_variable(name,shape,std):
+    return _bias_variable(name, shape, initializer=tf.truncated_normal_initializer(stddev=std,dtype=tf.float32),
+                          )
+def inference(x):
+    with tf.variable_scope('layer1') as scope:
+        weights = _weight_variable('weights',[784,256],0.04)
+        bias = _bias_variable('bias',[256],tf.constant_initializer(0.1))
+        layer1 = tf.nn.relu(tf.matmul(x,weights)+bias,name=scope.name)
+    with tf.variable_scope('layer2') as scope:
+        weights = _weight_variable('weights',[256,128],std=0.02)
+        bias = _bias_variable('bias',[128],tf.constant_initializer(0.2))
+        layer2 = tf.nn.relu(tf.matmul(layer1,weights)+bias,name=scope.name)
+    with tf.variable_scope('softmax_linear') as scope:
+        weights = _weight_variable('weights',[128,10],std=1/192.0)
+        bias = _bias_variable('bias',[10],tf.constant_initializer(0.0))
+        softmax_linear = tf.add(tf.matmul(layer2,weights),bias,name=scope.name)
+    return softmax_linear
+
+def loss(logits,labels):
+    print(labels.get_shape().as_list())
+    print(logits.get_shape().as_list())
+    labels = tf.cast(labels,tf.int64)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.argmax(labels,1),logits=logits,name = 'cross_entropy')
+    cross_entropy_mean  = tf.reduce_mean(cross_entropy,name = 'cross_entropy')
+    return cross_entropy_mean
+
+def train():
+    with tf.name_scope("input"):
+        x = tf.placeholder(tf.float32, shape=[None, 784], name='x')
+        y = tf.placeholder(tf.float32, shape=[None, 10], name='y')
+    softmax_linear = inference(x)
+    cost = loss(softmax_linear,y)
+    opt = tf.train.AdamOptimizer(0.001).minimize(cost)
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(softmax_linear, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+    #saver = tf.train.Saver()
+    with tf.Session() as sess:
+        print(y)
+        sess.run(tf.global_variables_initializer())
+        for epoch in range(FLAGS.traing_epoches):
+            avg_cost = 0.0
+            total_batch = int(mnist.train.num_examples/FLAGS.batch_size)
+            for _ in range(total_batch):
+                batch_xs,batch_ys = mnist.train.next_batch(FLAGS.batch_size)
+                sess.run(opt,feed_dict={x:batch_xs,y:batch_ys})
+                cost_ = sess.run(cost,feed_dict={x:batch_xs,y:batch_ys})
+            print(("%s epoch: %d,cost: %.6f")%(datetime.now(),epoch+1,cost_))
+            '''
+            if (epoch+1) % 5 == 0:
+                check_point_file = os.path.join(FLAGS.check_point_dir,'my_test_model')
+                saver.save(sess,check_point_file,global_step=epoch+1)
+            '''
+        graph = convert_variables_to_constants(sess,sess.graph_def,['layer2/layer2'])
+        tf.train.write_graph(graph,'.','graph.pb',as_text=False)
+        mean_accuary = sess.run(accuracy,{x:mnist.test.images,y:mnist.test.labels})
+        print("accuracy %3.f"%mean_accuary)
+    print()
+
+def main(_):
+   train()
+
+
+if __name__ == '__main__':
+  tf.app.run()
